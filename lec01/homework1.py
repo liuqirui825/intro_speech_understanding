0ad95fa85f4226d105b9043689f00db3a3cc68ae
liuqirui825 <liuqirui811@gmail.com>
2026-01-18 21:22:33 +0900

    saving changes

--- lec01/homework1.py
+++ lec01/homework1.py
@@ -12,6 +12,6 @@ string1 should be changed from "change this" to "test"
 string2 should be changed from "change this" to "hello"
 '''
 
-string1 = "change test"
+string1 = "test"
 
-string2 = "change hello"
+string2 = "hello"
