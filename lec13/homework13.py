e1b532cb161f37f1494aa9da4d1ad9c565ca52c1
liuqirui825 <liuqirui811@gmail.com>
2026-01-18 23:39:26 +0900

    saving changes

--- lec13/homework13.py
+++ lec13/homework13.py
@@ -45,5 +45,64 @@ def robot_voice(excitation, T0, frame_skip):
     gain (nframes) - gain for each frame
     e_robot (nframes*frame_skip) - excitation for the robot voice
     '''
-    raise RuntimeError("You need to write this part!")
+import numpy as np
+import librosa
 
+def lpc(speech, frame_length, frame_skip, order):
+    nframes = (len(speech) - frame_length) // frame_skip
+
+    A = np.zeros((nframes, order + 1))
+    excitation = np.zeros((nframes, frame_length))
+
+    for i in range(nframes):
+        start = i * frame_skip
+        frame = speech[start:start + frame_length]
+
+        a = librosa.lpc(frame, order=order)
+        A[i, :] = a
+
+        e = np.zeros(frame_length)
+        for n in range(order, frame_length):
+            pred = 0.0
+            for k in range(1, order + 1):
+                pred -= a[k] * frame[n - k]
+            e[n] = frame[n] - pred
+
+        excitation[i, :] = e
+
+    return A, excitation
+
+
+def synthesize(e, A, frame_skip):
+    nframes, order_plus1 = A.shape
+    order = order_plus1 - 1
+    length = nframes * frame_skip
+
+    synthesis = np.zeros(length)
+
+    for i in range(nframes):
+        a = A[i]
+        for n in range(frame_skip):
+            idx = i * frame_skip + n
+            synthesis[idx] = e[idx]
+            for k in range(1, order + 1):
+                if idx - k >= 0:
+                    synthesis[idx] -= a[k] * synthesis[idx - k]
+
+    return synthesis
+
+
+def robot_voice(excitation, T0, frame_skip):
+    nframes, frame_length = excitation.shape
+
+    gain = np.zeros(nframes)
+    e_robot = np.zeros(nframes * frame_skip)
+
+    for i in range(nframes):
+        gain[i] = np.sqrt(np.sum(excitation[i, -frame_skip:] ** 2) / frame_skip)
+
+        for n in range(frame_skip):
+            if n % T0 == 0:
+                e_robot[i * frame_skip + n] = gain[i]
+
+    return gain, e_robot
